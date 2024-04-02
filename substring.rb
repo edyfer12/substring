c880efb45d254a6171e06009908dc436eecac8f9
Edin <edyfer2@live.com.au>
2024-04-02 07:16:47 +0200

    Add comments and if statements

--- substring.rb
+++ substring.rb
@@ -2,5 +2,11 @@
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 "low", "own", "part", "partner", "sit"]
 
+#From the word "below", check if the substring of the word exists in the dictionary
+#Does original word match with the substring dictionary[0]? Yes
+#Print the substring dictionary[0]
+if "below".include?(dictionary[0])
+    puts dictionary[0]
+end
 #Invoke a method that will pass in inputs, string and dictionary and return a hash
-substrings("below", dictionary)
+#substrings("below", dictionary)
