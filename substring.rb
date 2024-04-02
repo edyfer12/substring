969f9ef6f7e888e7b0b31d7861b19d13daf25f72
Edin <edyfer2@live.com.au>
2024-04-02 15:29:15 +0200

    Indent the blocks of code

--- substring.rb
+++ substring.rb
@@ -6,26 +6,26 @@ binding.pry
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 "low", "own", "part", "partner", "sit"]
 def substrings(string, dictionary)
-#Create a hash collection to store words of dictionary as a key and number of words as value
-word_collection = {}
-#Convert the string into an array
-arr = "howdy partner, sit down! how's it going".split(" ")
-#Iterate through the dictionary array
-dictionary.each do |word|
-    #Set increment to 0
-    increment = 0
-    #Iterate through the array that are a set of inputs broken down
-    arr.each do |substring|
-        if substring.include?(word)
-            #Increment the variable by 1
-            increment+=1
-            #Set the key of the word collection to the increment value
-            word_collection[word] = increment
+    #Create a hash collection to store words of dictionary as a key and number of words as value
+    word_collection = {}
+    #Convert the string into an array
+    arr = "howdy partner, sit down! how's it going".split(" ")
+    #Iterate through the dictionary array
+    dictionary.each do |word|
+        #Set increment to 0
+        increment = 0
+        #Iterate through the array that are a set of inputs broken down
+        arr.each do |substring|
+            if substring.include?(word)
+                #Increment the variable by 1
+                increment+=1
+                #Set the key of the word collection to the increment value
+                word_collection[word] = increment
+            end
         end
     end
-end
-#Return the hash that will be displayed
-word_collection
+    #Return the hash that will be displayed
+    word_collection
 end
 #Invoke a method that will pass in inputs, string and dictionary and return a hash
 substrings("below", dictionary)
