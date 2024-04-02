e1aadb949da1b6f67035141c7f744edc3fbd4f4c
Edin <edyfer2@live.com.au>
2024-04-02 15:19:06 +0200

    Organize the loop and create hash

--- substring.rb
+++ substring.rb
@@ -6,15 +6,22 @@ binding.pry
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 "low", "own", "part", "partner", "sit"]
 #Create a hash collection to store words of dictionary as a key and number of words as value
+word_collection = {}
+arr = "howdy partner, sit down! how's it going".split(" ")
 #From the word "below", check if the substring of the word exists in the dictionary
 #Does original word match with the substring dictionary[0]? Yes
 #Add word as a key to the hash
 #Increment the word count of the key as a value
 #Repeat the process
 dictionary.each do |word|
-    if "below".downcase.include?(word)
-        puts word
+    increment = 0
+    arr.each do |substring|
+        if substring.include?(word)
+            increment+=1
+            word_collection[word] = increment
+        end
     end
 end
+puts word_collection
 #Invoke a method that will pass in inputs, string and dictionary and return a hash
 #substrings("below", dictionary)
