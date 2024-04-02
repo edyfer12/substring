e4f78d66e75c468284030c68e231d0ed0d4bbed3
Edin <edyfer2@live.com.au>
2024-04-02 07:38:48 +0200

    Add comments

--- substring.rb
+++ substring.rb
@@ -5,10 +5,11 @@ binding.pry
 #Create an array that will store a list of words similar to a dictionary
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 "low", "own", "part", "partner", "sit"]
-
+#Create a hash collection to store words of dictionary as a key and number of words as value
 #From the word "below", check if the substring of the word exists in the dictionary
 #Does original word match with the substring dictionary[0]? Yes
-#Print the substring dictionary[0]
+#Add word as a key to the hash
+#Increment the word count of the key as a value
 #Repeat the process
 dictionary.each do |word|
     if "below".downcase.include?(word)
