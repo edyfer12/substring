e2dae727dfd67f107624f37e646390d4eae234d4
Edin <edyfer2@live.com.au>
2024-04-02 06:46:21 +0200

    Add method and array

--- substring.rb
+++ substring.rb
@@ -1,3 +1,6 @@
 #Create an array that will store a list of words similar to a dictionary
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
+"low", "own", "part", "partner", "sit"]
 
 #Invoke a method that will pass in inputs, string and dictionary and return a hash
+substrings("below", dictionary)
